1576b35fee471e85bd8a1e77e690098e55053838
Fred Mitchell <fred.mitchell@gmx.de>
2017-09-05 16:19:58 +0200

    almost there...

--- ruby-proxy/proxy/server.rb
+++ ruby-proxy/proxy/server.rb
@@ -1,25 +1,45 @@
-class Server
+# coding: utf-8
 
-  INVOICE_QUEUE_NAME = "backend.invoices"
-  CLIENT_QUEUE_NAME = "backend.clients"
+INVOICE_QUEUE_NAME = "backend.invoices"
+CLIENT_QUEUE_NAME = "backend.clients"
 
+enable :sessions
 
-  def self.service(ch = nil)
-    @service ||= Server.new(ch)
+def connection
+  unless $connection
+    $connection = Bunny.new("amqp://#{PROXY_USER}:#{PROXY_PASSWORD}@#{PROXY_HOST}:#{PROXY_PORT}")
+    $connection.start
   end
-
-  private
-
-  def initialize(ch)
-    raise "ch must not be nil" if ch.nil?
-    @ch = ch
-  end
+  $connection
 end
+
+def channel
+  $channel = connection.create_channel unless $channel
+  $channel
+end
+
+def exchange
+  unless $exchange
+    $exchange = channel.default_exchange
+  end
+  $exchange
+end
+
+def get_clients
+  payload = {}.to_json
+  exchange.publish(payload, routing_key: CLIENT_QUEUE_NAME)
+end
+
+def get_invoices(id)
+  payload = { client_id: id }.to_json
+  exchange.publish(payload, routing_key: INVOICE_QUEUE_NAME)
+end
+
 
 get '/clients.json' do
-  'clients called'
+  get_clients
 end
 
-get '/invoices.json' do
-  'invoice called'
+get '/invoices.json/:client_id' do
+  get_invoices(params['client_id'])
 end
